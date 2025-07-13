21063bc1a6e4b157dda099a7a3d25d9435ed6529
DiegoGp227 <diego.gp227@gmail.com>
2025-07-13 00:46:48 -0500

    [ feat ] Integrated and functional log out button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,28 @@
 "use client";
 import Message from "@/components/Home/Message/index";
+import LogOut from "@/components/Home/LogOut";
 import { useEffect, useState } from "react";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import style from "./page.module.css";
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
+  const [token, setToken] = useState<boolean>(false);
 
   useEffect(() => {
     const storedToken = localStorage.getItem("token");
-
-    storedToken ? null : router.push("/login");
+    storedToken ? setToken(true) : router.push("/login");
   }),
-    [];
+    [token];
+
+  function changeTokenStatus() {
+    setToken(false);
+  }
 
   return (
     <div className={style.generalDiv}>
       <Message />
+      <LogOut changeTokenStatus={changeTokenStatus} />
     </div>
   );
 }
